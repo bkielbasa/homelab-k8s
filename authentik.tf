59ffd62c354480f7877f07abdd7074c73870eaee
Bartłomiej Klimczak <bartlomiej.klimczak@invia.com>
2026-05-04 00:56:29 +0200

    feat(authentik): raise helm_release timeout to 600s for slow image pull

--- authentik.tf
+++ authentik.tf
@@ -14,6 +14,8 @@ resource "helm_release" "authentik" {
   chart      = "authentik"
   version    = "2026.2.2"
 
+  timeout = 600
+
   depends_on = [
     kubectl_manifest.authentik_app_external_secret,
     kubectl_manifest.authentik_postgres_external_secret,
