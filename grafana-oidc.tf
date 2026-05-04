b24fd1757095d4bd655907499995786b0c61efc2
Bartłomiej Klimczak <bartlomiej.klimczak@invia.com>
2026-05-04 07:55:28 +0200

    feat(grafana-oidc): Authentik OIDC provider, application, grafana-admins group

---
+++ grafana-oidc.tf
@@ -0,0 +1,32 @@
+# Authentik group whose members get admin access to Grafana.
+# Membership managed manually in the Authentik UI.
+resource "authentik_group" "grafana_admins" {
+  name = "grafana-admins"
+}
+
+resource "authentik_provider_oauth2" "grafana" {
+  name               = "grafana"
+  client_id          = "grafana"
+  authorization_flow = data.authentik_flow.default_authorization_flow.id
+  invalidation_flow  = data.authentik_flow.default_invalidation_flow.id
+  signing_key        = data.authentik_certificate_key_pair.default.id
+
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict"
+      url           = "https://grafana.klimczak.xyz/login/generic_oauth"
+    },
+  ]
+
+  property_mappings = concat(
+    data.authentik_property_mapping_provider_scope.scopes.ids,
+    [authentik_property_mapping_provider_scope.groups.id],
+  )
+}
+
+resource "authentik_application" "grafana" {
+  name              = "Grafana"
+  slug              = "grafana"
+  protocol_provider = authentik_provider_oauth2.grafana.id
+  meta_launch_url   = "https://grafana.klimczak.xyz/login/generic_oauth"
+}
