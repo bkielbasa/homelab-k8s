1b8022be50f7ce8734313af8f8a9da2d6c580345
Bartłomiej Klimczak <bartlomiej.klimczak@invia.com>
2026-05-15 15:56:49 +0200

    feat(netbird): deploy NetBird Helm release (management, signal, dashboard, relay)

--- netbird.tf
+++ netbird.tf
@@ -6,3 +6,21 @@ resource "kubernetes_namespace" "netbird" {
     annotations = {}
   }
 }
+
+resource "helm_release" "netbird" {
+  name       = "netbird"
+  namespace  = kubernetes_namespace.netbird.metadata[0].name
+  repository = "https://netbirdio.github.io/helms"
+  chart      = "netbird"
+  version    = "1.9.0"
+
+  timeout = 600
+
+  depends_on = [
+    kubectl_manifest.netbird_external_secret,
+  ]
+
+  values = [
+    file("values/netbird.yaml")
+  ]
+}
